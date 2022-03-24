b1ddad7900e687fe81f13de7f074d98789cdf853
Yehudah <you@example.com>
2022-03-24 14:24:46 +0000

    make new route

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./routes";
 import About from "./routes/about";
 import Contact from "./routes/contact";
+import Portfolio from "./routes/Portfolio";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.render(
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact/> } />
+        <Route path="/Portfolio" element={<Portfolio />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
