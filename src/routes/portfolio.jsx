ff307eac9a88c2a518e2a186e9744839862053f8
Yehudah <you@example.com>
2022-03-24 15:12:25 +0000

    again

--- src/routes/portfolio.jsx
+++ src/routes/portfolio.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export default function portfolio() {
+export default function Portfolio() {
  return (
         <>
       <h1 className="home">portfolio</h1>
