f9e71fcb9c3a5fd2624d92266a30b6e2bd100571
Yehudah <you@example.com>
2022-03-24 14:53:02 +0000

    sort out problems

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,28 +7,5 @@ export default function Index() {
       <p>Welcome to my fabulous box</p>
       <Link to={"/about"}>Go to about</Link>
       <Link to={"/contact"}>Go to contact</Link>
-
-     className="Name">
-    <p>Yehudah Davidson</p>
-   
-
-<div> 
-<h3 className="title">Graphic Designer</h3>
-</div>
-
-<div>
-<h3 className="title2">  07552 749 955</h3>
-</div>
-
-<div className="sidebar">
-<ul>
-<li> <a href="home">Home</a> </li>
-<li> <a href="About">About</a> </li>
-<li> <a href="Portfolio">Portfolio</a></li>
-<li> <a href= "Contact">Contact</a></li>
-</ul>
-    </>
-    
-  );
-}
-
+      </>
+      )};
---
+++ src/routes/portfolio.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Index() {
+    return (
+        <>
+      <h1 className="home">Portfolio</h1>
+      <p>Welcome to my fabulous box</p>
+      <Link to={"/about"}>Go to about</Link>
+      <Link to={"/contact"}>Go to contact</Link>
+      <Link to={"/"}>Go to home</Link>
+      </>
+      )};
