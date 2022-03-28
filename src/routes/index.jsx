b666fcdfbd4b40e4d8e6cb3115997bcf7b55c7ad
Yehudah <you@example.com>
2022-03-28 16:21:31 +0100

    make sidebar

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,9 +5,15 @@ export default function Index() {
     <>
       <h1 className="home">Home Page</h1>
       <p>Welcome to my fabulous box</p>
-      <Link to={"/about"}>Go to about</Link>
-      <Link to={"/contact"}>Go to contact</Link>
-      <Link to={"/portfolio"}>Go to portfolio</Link>
+
+      <ul className="sidebar">
+      <li><Link to={"/about"}>Go to about</Link> </li>
+      <li><Link to={"/contact"}>Go to contact</Link> </li>
+      <li><Link to={"/portfolio"}>Go to portfolio</Link> </li>
+      </ul>
+    
+    
+    
       </>
       )
     }
