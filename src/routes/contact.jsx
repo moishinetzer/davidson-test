a3a6bb70e9c749b06d8f38d5bbbcebb42e5385c7
Yehudah <you@example.com>
2022-04-05 14:10:05 +0100

    l

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
       <p>Welcome to my favourite contacts</p>
       
       <ul className="sidebar">
-      <li><Link to={"/"}>home</Link></li>
+      <li><Link to={"/"}>home l</Link></li>
       <li><Link to={"/about"}>about</Link> </li>
       <li><Link to={"/contact"}>contact</Link> </li>
       <li><Link to={"/portfolio"}>portfolio</Link> </li>
