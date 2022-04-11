92b5d0d6bedd4bf7bc7fb214f70c7fdbb23c8c61
Yehudah <you@example.com>
2022-04-11 15:19:15 +0100

    do some updates

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,8 +6,15 @@ export default function Index() {
     
     <link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css"/>
 
+    <div className="topnav">
+        <a href="/">Home</a>
+        <a href="about">About</a>
+        <a href="contact">Contact</a>
+        <a href="portfolio">Portfolio</a>
+      </div> 
+
       <h1 className="home">Home Page</h1>
-      <p>Welcome to my fabulous box</p>
+      <p className="subtitle">Welcome to my fabulous box</p>
 
       <ul className="sidebar">
       <li><Link to={"/"}>home</Link></li>
@@ -16,6 +23,10 @@ export default function Index() {
       <li><Link to={"/portfolio"}>portfolio</Link> </li>
       </ul>
     
+           {/* <p className="aboutme">My name is Yehudah Davidson and I am a graphic designer.
+             My skills include brand and logo design, website development 
+             and image editing and compositing 
+           </p> */}
       </>
       )
     }
--- src/routes/portfolio.jsx
+++ src/routes/portfolio.jsx
@@ -5,9 +5,16 @@ export default function Portfolio() {
         <>
 <link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css"/>
 
+<div class="w3-sidebar w3-bar-block" style="width:25%">
+  <a href="/" class="w3-bar-item w3-button">Home</a>
+  <a href="about" class="w3-bar-item w3-button"></a>
+  <a href="portfolio" class="w3-bar-item w3-button">Portfolio</a>
+  <a href="contact" class="w3-bar-item w3-button">Contact</a>
+</div>
+
+      <div style="margin-left:25%">
       <h1 className="home">Portfolio</h1>
       <p>This is where I show everyone how amazing I am</p>
-     
       <ul className="sidebar">
       <li><Link to={"/"}>home</Link></li>
       <li><Link to={"/about"}>about</Link> </li>
@@ -15,6 +22,11 @@ export default function Portfolio() {
       <li><Link to={"/portfolio"}>portfolio</Link> </li>
       </ul>
 
+      <img src="src/images/logo.png" alt="my first logo" />
+
+</div>
+
       </>
       )
  }
+ 
