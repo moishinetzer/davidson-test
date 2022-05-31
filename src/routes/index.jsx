39aed9ee6d118bc5ea08e08c996bd8fe110a9712
Moishi Netzer <moishinetzer@gmail.com>
2022-05-31 17:50:32 +0300

    Add tailwind

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,38 +3,45 @@ import { Link } from "react-router-dom";
 export default function Index() {
   return (
     <>
+      <link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css" />
 
-    <link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css"/>
-  
-      
-    <div className="topnav">
+      <div className="topnav">
         <a href="/">Home</a>
         <a href="about">About</a>
         <a href="contact">Contact</a>
         <a href="portfolio">Portfolio</a>
-      </div> 
+      </div>
 
-    <div className="topnav">
+      <div className="topnav">
         <a href="/">Home</a>
         <a href="about">About</a>
         <a href="contact">Contact</a>
         <a href="portfolio">Portfolio</a>
-      </div> 
+      </div>
 
       <h1 className="home">Home Page</h1>
       <p className="subtitle">Welcome to my fabulous box</p>
 
       <ul className="sidebar">
-      <li><Link to={"/"}>home</Link></li>
-      <li><Link to={"/about"}>about</Link> </li>
-      <li><Link to={"/contact"}>contact</Link> </li>
-      <li><Link to={"/portfolio"}>portfolio</Link> </li>
+        <li>
+          <Link to={"/"}>home</Link>
+        </li>
+        <li>
+          <Link to={"/about"}>about</Link>{" "}
+        </li>
+        <li>
+          <Link to={"/contact"}>contact</Link>{" "}
+        </li>
+        <li>
+          <Link to={"/portfolio"}>portfolio</Link>{" "}
+        </li>
       </ul>
-    
-           {/* <p className="aboutme">My name is Yehudah Davidson and I am a graphic designer.
-             My skills include brand and logo design, website development 
-             and image editing and compositing 
-           </p> */}
-      </>
-      )
-    }
+
+      <p className="aboutme bg-black text-white">
+        My name is Yehudah Davidson and I am a graphic designer. My skills
+        include brand and logo design, website development and image editing and
+        compositing
+      </p>
+    </>
+  );
+}
--- src/routes/portfolio.jsx
+++ src/routes/portfolio.jsx
@@ -1,36 +1,6 @@
 import { Link } from "react-router-dom";
 
 export default function Portfolio() {
-<<<<<<< HEAD
- return (
-        <>
-<link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css"/>
-<link rel="stylesheet" href="w3.css"/>
-
-<div className="topnav">
-        <a href="/">Home</a>
-        <a href="about">About</a>
-        <a href="contact">Contact</a>
-        <a href="portfolio">Portfolio</a>
-      </div>
-
-      <div style="margin-left:25%">
-      <h1 className="home">Portfolio</h1>
-      <p>This is where I show everyone how amazing I am</p>
-      </div>
-      
-      <ul className="sidebar">
-      <li><Link to={"/"}>home</Link></li>
-      <li><Link to={"/about"}>about</Link> </li>
-      <li><Link to={"/contact"}>contact</Link> </li>
-      <li><Link to={"/portfolio"}>portfolio</Link> </li>
-      </ul>
-
-      </>
-      )
- }
- 
-=======
   return (
     <>
       <link rel="stylesheet" href="https://use.typekit.net/rwh2pbj.css" />
@@ -71,4 +41,3 @@ export default function Portfolio() {
     </>
   );
 }
->>>>>>> bc99ca786015245e9a0401ac9ad419b1dc22834b
